ad0e3b315474dcbf41ade19596a837c4db5dc164
backup123 <tillforever123@gmail.com>
2015-04-04 02:40:30 -0700

    asd

---
+++ CMakeFiles/Surf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Surf.dir/surf.cpp.o"
+  "Surf.pdb"
+  "Surf"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Surf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
